c723ef1e044838252aadacd29ab6ded57fe6d09f
Samuel <samueltanwj@gmail.com>
2025-10-01 23:07:23 +0800

    Integrate add remark functionalities

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -16,6 +16,10 @@ public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
 
+    @FXML
+    private Label remark;
+
+
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
      * As a consequence, UI elements' variable names cannot be set to such keywords
